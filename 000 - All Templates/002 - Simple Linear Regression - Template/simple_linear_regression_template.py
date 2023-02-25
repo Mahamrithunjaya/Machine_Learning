f84ff3f53985442d1fcd9f8632d4967bbb4a6481
Mahamrithunjaya <sahasubhajitias59@gmail.com>
2023-02-26 02:50:56 +0530

    Changes Added

--- 000 - All Templates/002 - Simple Linear Regression - Template/simple_linear_regression_template.py
+++ 000 - All Templates/002 - Simple Linear Regression - Template/simple_linear_regression_template.py
@@ -35,21 +35,3 @@ regressor.fit(X_train, y_train)
 """## Predicting the Test set results"""
 
 y_pred = regressor.predict(X_test)
-
-"""## Visualising the Training set results"""
-
-plt.scatter(X_train, y_train, color = 'red')
-plt.plot(X_train, regressor.predict(X_train), color = 'blue')
-plt.title('Training set)
-plt.xlabel('X')
-plt.ylabel('Y')
-plt.show()
-
-"""## Visualising the Test set results"""
-
-plt.scatter(X_test, y_test, color = 'red')
-plt.plot(X_train, regressor.predict(X_train), color = 'blue')
-plt.title('Test set')
-plt.xlabel('X')
-plt.ylabel('Y')
-plt.show()
